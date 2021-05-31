0302c44bb5bcb349f130249c15cc46aff4614289
Yuriy Prosvirnin <prosvirniny@gmail.com>
2021-05-31 23:00:50 +0300

    diff views for single/multi/range answers

--- Quiz/ViewControllers/QuestionViewController.swift
+++ Quiz/ViewControllers/QuestionViewController.swift
@@ -12,6 +12,7 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var questionStack: UIStackView!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var buttonNext: UIButton!
     
     var questionIndex: Int = 0
     var questions: [Question] = Question.all.shuffled()
@@ -62,17 +63,14 @@ class QuestionViewController: UIViewController {
             answerRow.axis = .horizontal
             answerRow.alignment = .center
             answerRow.distribution = .fill
-            answerRow.spacing = 20
+            answerRow.spacing = 0
             
-            let button = UIButton()
+            let button = UIButton(type: .roundedRect)
             button.setTitle(answer.text, for: [])
-            button.setTitleColor(UIColor.label, for: [])
-            button.setTitleShadowColor(UIColor.lightGray, for: [])
-            button.isSelected = true
-            button.layer.cornerRadius = 5
-            button.tag = n
-            
-            button.isEnabled = true
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+            button.setTitleColor(UIColor.darkText, for: [])
+            button.titleShadowColor(for: [])
+            button.isSelected = (n == 1)
             
             answerRow.addArrangedSubview(button)
             
@@ -81,21 +79,70 @@ class QuestionViewController: UIViewController {
         
     }
     
+    private func addAnswersRange(_ question: Question) {
+        
+        let currentAnswer = question.answers[0]
+        
+        let answerRow = UIStackView()
+        answerRow.axis = .vertical
+        answerRow.alignment = .fill
+        answerRow.distribution = .fill
+        answerRow.spacing = 0
+        
+        let valueTitle = UILabel()
+        valueTitle.text = currentAnswer.text
+        valueTitle.textAlignment = .center
+        valueTitle.font = UIFont.systemFont(ofSize: 28)
+        
+        let slider = UISlider()
+        slider.minimumValue = 0
+        slider.maximumValue = Float(question.answers.count)
+        
+        let minMaxRow = UIStackView()
+        minMaxRow.axis = .horizontal
+        minMaxRow.alignment = .fill
+        minMaxRow.distribution = .fillEqually
+        minMaxRow.spacing = 0
+
+        let minValueTitle = UILabel()
+        minValueTitle.text = question.answers[0].text
+        minValueTitle.textAlignment = .left
+        minValueTitle.font = UIFont.systemFont(ofSize: 20)
+
+        let maxValueTitle = UILabel()
+        maxValueTitle.text = question.answers[question.answers.count - 1].text
+        maxValueTitle.textAlignment = .right
+        maxValueTitle.font = UIFont.systemFont(ofSize: 20)
+
+        minMaxRow.addArrangedSubview(minValueTitle)
+        minMaxRow.addArrangedSubview(maxValueTitle)
+        
+        answerRow.addArrangedSubview(valueTitle)
+        answerRow.addArrangedSubview(slider)
+        answerRow.addArrangedSubview(minMaxRow)
+        
+        questionStack.addArrangedSubview(answerRow)
+        
+    }
     
     
     private func updateUI() {
         
         navigationItem.title = "Вопрос № \(questionIndex + 1)"
-        progressBar.setProgress(Float(questionIndex + 1) / Float(questions.endIndex), animated: true)
+        
+        progressBar.setProgress(0.1, animated: false)
+        
+        print(Float(questionIndex + 1), Float(questions.endIndex))
+        print(Float(questionIndex + 1) / Float(questions.endIndex))
         
         let question = questions[questionIndex]
         
         questionText.text = question.text
         
         switch question.style {
-        case .multi: addAnswersSingle(question)
+        case .multi: addAnswersMulti(question)
         case .single: addAnswersSingle(question)
-        case .range: addAnswersSingle(question)
+        case .range: addAnswersRange(question)
         }
         
     }
